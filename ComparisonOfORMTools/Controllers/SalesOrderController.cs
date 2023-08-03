6c004167ddaedd6118d59ac0bffac8c9f0b454ef
Merve Doğan <merve36540@gmail.com>
2023-08-03 19:44:35 +0300

    Downloaded orm tools, made db connection for dapper and run sample query.

---
+++ ComparisonOfORMTools/Controllers/SalesOrderController.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ComparisonOfORMTools.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesOrderController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public SalesOrderController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]   
+        public async Task<ActionResult<List<SalesOrderDetail>>> GetAllOrders()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var orders = await connection.QueryAsync<SalesOrderDetail>("select *  from Sales.SalesOrderDetail WHERE ProductID=870;");
+            return Ok(orders);
+        } 
+    }
+}
---
+++ ComparisonOfORMTools/SalesOrderDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace ComparisonOfORMTools
+{
+    public class SalesOrderDetail
+    {
+        public int SalesOrderID { get; set; }
+        public int SalesOrderDetailID { get; set; }
+        public string CarrierTrackingNumber { get; set; } = string.Empty;
+        public int OrderQty { get; set; }
+        public int ProductID { get; set; }
+        public int SpecialOfferID { get; set; }
+        public float UnitPrice { get; set; }
+        public float UnitPriceDiscount { get; set; }
+        public int LineTotal { get; set; }
+        public Guid rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
