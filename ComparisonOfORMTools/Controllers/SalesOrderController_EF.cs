77d3b83a7b57c1fa73c3a70c6fd9295c6480f600
Merve Doğan <merve36540@gmail.com>
2023-08-07 01:36:40 +0300

    entity framework done with selected query

---
+++ ComparisonOfORMTools/Controllers/SalesOrderController_EF.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComparisonOfORMTools.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesOrderController_EF : ControllerBase
+    {
+
+        private readonly AdventureWorks2022Context _context;
+
+        public SalesOrderController_EF(AdventureWorks2022Context context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> getOrderSales()
+        {
+            var filteredSales = await _context.SalesOrderDetail
+                           .Where(s => s.ProductId == 870)
+                           .ToListAsync();
+
+            return Ok(filteredSales);
+        }
+    }
+}
---
+++ ComparisonOfORMTools/Models/MockDatum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparisonOfORMTools.Models;
+
+public partial class MockDatum
+{
+    public int? Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? IpAddress { get; set; }
+}
