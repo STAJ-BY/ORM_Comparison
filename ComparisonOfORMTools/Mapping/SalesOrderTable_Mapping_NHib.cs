d5f59b5d5c86165897c330f965e9c29e17f38f35
Merve Doğan <merve36540@gmail.com>
2023-08-07 15:54:28 +0300

    NHibernate Mapping added

---
+++ ComparisonOfORMTools/Mapping/SalesOrderTable_Mapping_NHib.cs
@@ -0,0 +1,26 @@
+﻿using ComparisonOfORMTools.Models;
+using FluentNHibernate.Mapping;
+
+namespace ComparisonOfORMTools.Mapping
+{
+    public class SalesOrderTable_Mapping_NHib: ClassMap<Models.SalesOrder_NHib>
+    {
+        public SalesOrderTable_Mapping_NHib()
+        {
+            Id(x =>  x.SalesOrderDetailID);
+            Map(x => x.SalesOrderID).Not.Nullable();
+            Map(x => x.CarrierTrackingNumber).Not.Nullable();
+            Map(x => x.OrderQty).Not.Nullable();
+            Map(x => x.SalesOrderID).Not.Nullable();
+            Map(x => x.ProductID).Not.Nullable();
+            Map(x => x.SpecialOfferID).Not.Nullable();
+            Map(x => x.UnitPrice).Not.Nullable();
+            Map(x => x.UnitPriceDiscount).Not.Nullable();
+            Map(x => x.LineTotal).Not.Nullable();
+            Map(x => x.rowguid).Not.Nullable();
+            Map(x => x.ModifiedDate).Not.Nullable();
+            Table("SalesOrderDetail");
+
+        }
+    }
+}
---
+++ ComparisonOfORMTools/Models/SalesOrder_NHib.cs
@@ -0,0 +1,19 @@
+﻿namespace ComparisonOfORMTools.Models
+{
+    public class SalesOrder_NHib
+    {
+        public virtual int SalesOrderID { get; set; }
+        public virtual int SalesOrderDetailID { get; set; }
+        public virtual string CarrierTrackingNumber { get; set; } 
+        public virtual int OrderQty { get; set; }
+        public virtual int ProductID { get; set; }
+        public virtual int SpecialOfferID { get; set; }
+        public virtual float UnitPrice { get; set; }
+        public virtual float UnitPriceDiscount { get; set; }
+        public int LineTotal { get; set; }
+        public virtual Guid rowguid { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+
+
+    }
+}
